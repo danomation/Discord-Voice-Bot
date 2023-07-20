e3455c93f2a41df4661c61c7c92d16bbf8e70637
Dan <dluttrell87@gmail.com>
2023-07-20 09:46:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import { Client, GatewayIntentBits, Events } from 'discord.js';
 import { joinVoiceChannel, SpeakingMap} from '@discordjs/voice';
-//import { writeFileSync } from 'fs';
-//import pkg from '@discordjs/opus';
-//const { OpusEncoder } = pkg;
 
 // set these:
 // your voice channel
@@ -11,10 +8,6 @@ const voice_channel_id = ''
 const bot_py_id = ''
 // your discord token
 const discord_token = ''
-
-// not used
-//  Maps userIds to their speaking states.
-//const speakingStates = new Map();
 
 const client = new Client({
   intents: [
@@ -41,36 +34,9 @@ client.once('ready', async () => {
     // ironically this is the trigger for the main bot right now. I know it's hacky as heck.
     if (myuser != bot_py_id) channel.send("!record "+ myuser);
     console.log(myuser + " started speaking");
-
-    //const mytest = Array.from(connection.receiver.speaking.users.keys());
-    //mytest.forEach(element => {
-    //  speakingStates.set(element, true);
-    //});
-    //console.log(speakingStates);
   });
-
-// didn't need it
-//  connection.receiver.speaking.on('end', async() => {
-//    const myuser = Array.from(connection.receiver.speaking.users.keys())[0];
-//    console.log(myuser + " stopped speaking");
-//    const mytest = Array.from(connection.receiver.speaking.users.keys());
-//    mytest.forEach(element => {
-//      speakingStates.set(element, false);
-//    });
-//    console.log(speakingStates);
-//  });
-
-
-//  speakingMap.on('start', (userId) => {
-//    speakingStates.set(myuser, true);
-//    writeFileSync('speakingStatus.json', JSON.stringify(Array.from(speakingStates.entries()), null, 2));
-//  });
-
-//  speakingMap.on('end', (userId) => {
-//    speakingStates.set(userId, false);
-//    writeFileSync('speakingStatus.json', JSON.stringify(Array.from(speakingStates.entries()), null, 2));
-//  });
-
+  
+// might not need this? Idk I'm a noob
 });
 
 
